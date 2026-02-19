1b08a7e2785acb2294ccfb55c526e3abf940c248
hamzajail007-gif <hamzajail007@gmail.com>
2026-02-19 07:20:31 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Hamza botz',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "Hamza Bot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '92339 6386670', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
